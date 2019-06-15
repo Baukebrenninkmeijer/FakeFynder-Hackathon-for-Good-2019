1cebf9a162b57e3cc062365b112d6cefb0f23787
sbalk <stijn.balk@gmail.com>
2019-06-15 15:59:14 +0200

    changes to download_yt

--- app/download_yt.py
+++ app/download_yt.py
@@ -3,4 +3,6 @@ from pytube import YouTube
 def download_video(link):
     yt = YouTube(link)
     yt.streams.filter(progressive=True).order_by('resolution').desc().first().download()
-    
+
+# if __name__ == "__main__":
+# 	download_video('https://www.youtube.com/watch?v=cQ54GDm1eL0')
